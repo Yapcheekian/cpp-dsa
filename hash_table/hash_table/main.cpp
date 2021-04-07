053604d7922d42439128845b2bbef184816119e5
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-04-07 20:23:15 +0800

    first commit

---
+++ hash_table/hash_table/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hash_table
+//
+//  Created by Yap Chee Kian on 2021/4/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
