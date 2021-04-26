b066434133c251fd3e315468c949b76bfbeb8356
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-04-26 22:58:45 +0800

    add binary search

--- Array/Array/main.c
+++ Array/Array/main.c
@@ -73,12 +73,45 @@ int LinearSearch(struct Array *arr, int key)
     return -1;
 }
 
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l = 0;
+    h = arr.length - 1;
+    while(l <= h)
+    {
+        mid = (l + h) / 2;
+        if(key == arr.A[mid])
+            return mid;
+        else if(key < arr.A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return -1;
+}
+
+int RBinarySearch(int a[], int l, int h, int key)
+{
+    if(l <= h)
+    {
+        int mid = (l + h) / 2;
+        if(key == a[mid])
+            return mid;
+        else if(key < a[mid])
+            return RBinarySearch(a, l, mid - 1, key);
+        else
+            return RBinarySearch(a, mid + 1, h, key);
+    }
+    return -1;
+}
+
 int main(int argc, const char * argv[]) {
     
     struct Array arr = {{2,4,7,8,9}, 10, 5};
     Insert(&arr, 3, 100);
     Display(arr);
-    printf("\nlinear search %d\n", LinearSearch(&arr, 7));
+    printf("\nlinear search %d\n", BinarySearch(arr, 9));
     
     return 0;
 }
