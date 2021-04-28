5414f4c1f27445a3e797b534bf1ef00c3cf49f82
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-04-28 09:04:20 +0800

    add stack

---
+++ Stack/Stack/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    int size;
+    int top;
+    int *S;
+};
+
+void create(struct Stack *st)
+{
+    printf("Enter size\n");
+    scanf("%d", &st->size);
+    st->top = -1;
+    st->S = (int *)malloc(st->size * sizeof(int));
+}
+
+void Display(struct Stack st)
+{
+    int i;
+    for(i = st.top; i >= 0; i--)
+        printf("%d ", st.S[i]);
+    printf("\n");
+}
+
+void Push(struct Stack *st, int x)
+{
+    if(st->top == st->size - 1)
+        printf("Stack overflow\n");
+    else
+    {
+        st->top++;
+        st->S[st->top] = x;
+    }
+}
+
+int Pop(struct Stack *st)
+{
+    int x = -1;
+    
+    if(st->top == -1)
+        printf("Stack underflow\n");
+    else
+    {
+        x = st->S[st->top--];
+    }
+    return x;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    struct Stack st;
+    create(&st);
+    Push(&st, 10);
+    Push(&st, 20);
+    Push(&st, 30);
+    Push(&st, 40);
+    Push(&st, 50);
+    Display(st);
+    
+    return 0;
+}
